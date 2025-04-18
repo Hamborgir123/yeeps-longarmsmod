2b02e978589b9317493ee6974e84b3147e116c14
Hamborgir123 <granttplace@yahoo.com>
2025-04-17 23:03:48 -0400

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Update this path to match your NDK folder name
+export NDK_PATH=$HOME/android-ndk-r25c
+
+cmake -B build -S . \
+-DCMAKE_TOOLCHAIN_FILE=$NDK_PATH/build/cmake/android.toolchain.cmake \
+-DANDROID_ABI=arm64-v8a \
+-DANDROID_PLATFORM=android-24
+
+cmake --build build
+
